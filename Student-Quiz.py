e1a2d4b798f1f3d5bec0120aafd9daec7415e989
RAJ KUMAR A <146355426+RAJ322622@users.noreply.github.com>
2025-04-15 21:00:13 +0530

    Update Student-Quiz.py

--- Student-Quiz.py
+++ Student-Quiz.py
@@ -452,7 +452,8 @@ elif choice == "Professor Panel":
         if st.button("Verify Key"):
             if secret_key == PROFESSOR_SECRET_KEY:
                 st.session_state.prof_verified = True
-                st.experimental_rerun()
+                st.rerun()
+
             else:
                 st.error("Invalid secret key! Access denied.")
     else:
